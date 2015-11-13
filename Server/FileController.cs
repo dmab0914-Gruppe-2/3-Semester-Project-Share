3f5001f9f0b4ee353572a88be91ff30ffc8b2da4
Nicklas <welle93@gmail.com>
2015-11-13 11:52:55 +0100

    FileController added

---
+++ Server/FileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class FileController:IFileControler
+    {
+        public List<Library.File> AllFilesForProject(int projectID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Library.FileVersion> AllVersionsForFile(int fileID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFile(int fileID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFile(int fileID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditFile(int fileID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
