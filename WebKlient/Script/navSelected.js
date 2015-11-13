c97d3221fadc567586bc50dbeb618bef2ba77fcc
Nicklas <welle93@gmail.com>
2015-11-13 09:00:19 +0100

    webClient

---
+++ WebKlient/Script/navSelected.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    var str = location.href.toLowerCase();
+    $('#menu li a').each(function () {
+        if (str.indexOf(this.href.toLowerCase()) > -1) {
+            $("li.highlight").removeClass("highlight");
+            $(this).parent().addClass("highlight");
+        }
+    });
+    $('li.highlight').parents().each(function () {
+
+        if ($(this).is('li')) {
+            $(this).addClass("highlight");
+        }
+    });
+})
