e7452df5304311c2e9b6f7e3318a79b879d29d92
Jacob Pedersen (Q11x) <q11x@q11x.net>
2015-11-18 07:58:50 +0100

    A bit more work on UnitTest for 21

--- UnitTestProject/AddFileToProject21Test.cs
+++ UnitTestProject/AddFileToProject21Test.cs
@@ -9,36 +9,31 @@ namespace UnitTestProject
     public class AddFileToProject21Test
     {
         [TestMethod]
-        public void FindProject()
+        public void GetProject()
         {
             const int id = 1;
-            const string title = "The Big Project";
-            const string description = "It's a fairly big project?";
-            const string projectFolder = "theBigproject";
-            const string userName = "FedeFrank";
-            const string password = "1234";
-            const UserType type = UserType.User;
 
-            User projectAdministrator = new User(userName, password, type);
             ProjectController projectController = new ProjectController();
-            projectController.addProject(new Project(id, title, description, projectFolder, projectAdministrator));
-            Project project = projectController.FindProject(id);
-
-            Assert.AreEqual(title, project.Title);
-            Assert.AreEqual(description, project.Description);
-            Assert.AreEqual(projectFolder, project.ProjectFolder);
+            Project project = projectController.GetProject(id);
+            Assert.IsNotNull(project);
         }
 
         [TestMethod]
         public void UploadFileToProject()
         {
-            string title = "Fikse filen";
+            int projectId = 1;
+            string title = "Fiske filen";
             string description = "Generelt bare meget om fisk..";
-            System.IO.File actualFile = null;
+
+            ProjectController projectController = new ProjectController();
+            Project project = projectController.GetProject(projectId);
+            
+            projectController.addFileToProject(projectId, file); //Will throw exception if file already exists.
+            string filePath = "/" + projectId + "/";
+            //System.IO.File actualFile = null;
             //TODO This will have to be changed, as we have currently no idea how sending files from client to server would work..
 
             File file = new File(title, description);
-            ProjectController projectController = new ProjectController();
             projectController.addFileToProject(projectId, file, actualFile); //TODO This ain't going to work..
         }
     }
