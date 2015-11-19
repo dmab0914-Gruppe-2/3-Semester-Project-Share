ce39e1d188fc6ebb8a64aa8c9f831549af0f6bce
SplintDK <splint@splintnet.dk>
2015-11-19 12:33:45 +0100

    Changed type of VersionNumber from double to int

--- Library/FileVersion.cs
+++ Library/FileVersion.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.Mapping;
 using System.Linq;
 using System.Text;
 
 namespace Library
 {
+    [Table(Name = "FileVersion")]
     public class FileVersion
     {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         public int Id { get; set; }
+        [Column]
         public string FilePath { get; set; }
-        public double VersionNumber { get; set; }
+        [Column]
+        public int VersionNumber { get; set; }
+        [Column]
         public DateTime TimeStamp { get; set; }
+        [Column]
         public User Owner { get; set; }
 
-        public FileVersion(string filePath, double versionNumber, User owner)
+        public FileVersion(string filePath, int versionNumber, User owner)
         {
             FilePath = filePath;
             VersionNumber = versionNumber;
@@ -21,7 +28,7 @@ namespace Library
             Owner = owner;
         }
 
-        public FileVersion(int id, string filePath, double versionNumber, User owner)
+        public FileVersion(int id, string filePath, int versionNumber, User owner)
         {
             Id = id;
             FilePath = filePath;
@@ -30,7 +37,7 @@ namespace Library
             Owner = owner;
         }
 
-        public FileVersion(int id, string filePath, double versionNumber, DateTime timeStamp, User owner)
+        public FileVersion(int id, string filePath, int versionNumber, DateTime timeStamp, User owner)
         {
             Id = id;
             FilePath = filePath;
