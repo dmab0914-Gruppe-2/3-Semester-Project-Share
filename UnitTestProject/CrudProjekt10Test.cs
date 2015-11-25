7a6f61b75d0ba6d7562c217ad5796f9497d65f67
SplintDK <splint@splintnet.dk>
2015-11-25 12:47:52 +0100

    CrudProjekt test

---
+++ UnitTestProject/CrudProjekt10Test.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+using Server;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CrudProjekt10Test
+    {
+        const int id = 1;
+        const string title = "The Big Project";
+        const string description = "It's a fairly big project?";
+        const string projectFolder = @"c:\";
+        User projectAdminUser = new User
+        {
+            Id = 1,
+            UserName = "arm",
+            PassWord = "1234",
+            Type = UserType.Administrator
+        };
+
+        ProjectController projectController = new ProjectController();
+#region CreateProject
+        [TestMethod]
+        public void TestCreateProjectSuccess()
+        {
+
+            ProjectReturnType returnValue = projectController.AddProject(title, description, projectFolder, projectAdminUser);
+            Assert.AreEqual(ProjectReturnType.Success, returnValue);
+        }
+
+        [TestMethod]
+        public void TestCreateProjectFailTitle()
+        {
+
+            ProjectReturnType returnValue = projectController.AddProject(null, description, projectFolder, projectAdminUser);
+            Assert.AreEqual(ProjectReturnType.TitleMissing, returnValue); 
+        }
+
+        [TestMethod]
+        public void TestCreateProjectFailDescription()
+        {
+            ProjectReturnType returnValue = projectController.AddProject(title, null, projectFolder, projectAdminUser);
+            Assert.AreEqual(ProjectReturnType.DescriptionMissing, returnValue);
+        }
+
+        [TestMethod]
+        public void TestCreateProjectFailProjectFolder()
+        {
+            ProjectReturnType returnValue = projectController.AddProject(title, description, null, projectAdminUser);
+            Assert.AreEqual(ProjectReturnType.ProjectFolderMissing, returnValue);
+        }
+
+        [TestMethod]
+        public void TestCreateProjectFailUser()
+        {
+            ProjectReturnType returnValue = projectController.AddProject(title, description, projectFolder, null);
+            Assert.AreEqual(ProjectReturnType.ProjectAdministratorUserMissing, returnValue);
+        }
+#endregion
+
+        [TestMethod]
+        public void TestGetAllProjects()
+        {
+            List<Project> projects = projectController.GetAllProjects();
+            Assert.IsNotNull(projects);
+        }
+    }
+}
