a21e89a9a0dd94f4b9d06d01d2bf231f1351c3c3
Jacob Pedersen (Q11x) <q11x@q11x.net>
2015-11-25 14:06:45 +0100

    removed test code from Server Main

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,34 +10,7 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hit a button when ready..");
-            System.Console.ReadLine();
-            Console.WriteLine("");
-            DbContext dbContext = new DbContext();
-            //var d = dbContext.Users.Where(x => x.Username.Contains("jo"));
-            //foreach (User user in d)
-            //{
-            //    Console.WriteLine("ID: " + user.Id);
-            //    Console.WriteLine("Username: " + user.Username);
-            //    Console.WriteLine("Password: " + user.Password);
-            //    Console.WriteLine("");
-            //}
-
-            var d = dbContext.Projects.Where(x => x.Id.Equals(1));
-            foreach (Project project in d)
-            {
-                Console.WriteLine("ID: " + project.Id);
-                Console.WriteLine("Title: " + project.Title);
-                Console.WriteLine("Members: ");
-                foreach (User member in project.ProjectMembers)
-                {
-                    Console.WriteLine("Member: " + member.Username);
-                }
-                Console.WriteLine("");
-            }
-
-            Console.WriteLine("DONE!");
-            Console.ReadLine();
+            
         }
     }
 }
