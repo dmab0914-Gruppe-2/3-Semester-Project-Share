fff239ae23255649aa628934eedefdbb9f00e4a6
SplintDK <splint@splintnet.dk>
2015-11-25 11:43:26 +0100

    new Interface for DbProject

---
+++ Server/Database/IDbProject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace Server.Database
+{
+    interface IDbProject
+    {
+        int AddProject(string title, string description, string projectFolder, User projectAdministratorUser);
+        int RemoveProject(int id);
+        Project GetProject(int id);
+        int UpdateProject(int id, string title, string description, string projectFolder, User projectAdministratorUser);
+        List<Project> GetAllProjects();
+
+    }
+}
