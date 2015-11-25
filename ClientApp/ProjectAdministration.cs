a668bedbb4544f79a37099bb2fcb124b789790b1
SplintDK <splint@splintnet.dk>
2015-11-25 12:46:47 +0100

    Added new Window
    Project Administration with listView and Service reference

---
+++ ClientApp/ProjectAdministration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+    public partial class ProjectAdministration : Form
+    {
+        public ProjectAdministration()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectAdministration_Load(object sender, EventArgs e)
+        {
+            //ListView load peoperties...
+            listView_Projects.GridLines = true;
+            listView_Projects.View = View.Details;
+            listView_Projects.FullRowSelect = true;
+            //ListView columns
+            listView_Projects.Columns.Add("Id", 30);
+            listView_Projects.Columns.Add("Project", 150);
+            listView_Projects.Columns.Add("Description", 200);
+            
+
+        }
+
+    }
+}
