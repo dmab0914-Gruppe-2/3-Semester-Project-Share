ceaac8d3ba0672a6755d2124cc24160a4721916f
SplintDK <splint@splintnet.dk>
2015-11-25 12:48:05 +0100

    ProjectController service

---
+++ WebService/ServiceProjectController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Server;
+
+namespace WebService
+{
+
+    public class ServiceProjectController : IServiceProjectController
+    {
+        ProjectController projectController = new ProjectController();
+
+        public Library.ProjectReturnType AddProject(string title, string description, string projectFolder, Library.User projectAdministratorUser)
+        {
+            return projectController.AddProject(title, description, projectFolder, projectAdministratorUser);
+        }
+
+
+        public List<Library.Project> GetAllProjects()
+        {
+            return projectController.GetAllProjects();
+        }
+    }
+}
