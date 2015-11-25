199a6f1b56865931077585404b3272a823e2b856
SplintDK <splint@splintnet.dk>
2015-11-25 13:36:05 +0100

    Added TestSetup

--- UnitTestProject/CrudProjekt10Test.cs
+++ UnitTestProject/CrudProjekt10Test.cs
@@ -22,6 +22,26 @@ namespace UnitTestProject
         };
 
         ProjectController projectController = new ProjectController();
+
+#region SetupTest
+        [TestInitialize]
+        public void Setup()
+        {
+            Project project1 = new Project(1, "World Domination", "Self Explainatory", @"c:\", projectAdminUser);
+            Project project2 = new Project(2, "Bridge Construction", "Bridge over Klisterkanalen", @"C:\Projects\BridgeConstruction", projectAdminUser);
+            Project project3 = new Project(3, "Recruiting", "Get more members", @"C:\Projects\Recruiters", projectAdminUser);
+            Project project4 = new Project(4, "Server code", "Backend server code", @"C:\Projects\Backend", projectAdminUser);
+            Project project5 = new Project(5, "Life and Universe", "What is 42?", @"C:\Projects\42", projectAdminUser);
+            projectController.AddProject(project1);
+            projectController.AddProject(project2);
+            projectController.AddProject(project3);
+            projectController.AddProject(project4);
+            projectController.AddProject(project5);
+
+        }
+#endregion
+
+
 #region CreateProject
         [TestMethod]
         public void TestCreateProjectSuccess()
@@ -61,11 +81,16 @@ namespace UnitTestProject
         }
 #endregion
 
+
+#region GetAllProjects
         [TestMethod]
         public void TestGetAllProjects()
         {
             List<Project> projects = projectController.GetAllProjects();
             Assert.IsNotNull(projects);
         }
+#endregion
+
+
     }
 }
