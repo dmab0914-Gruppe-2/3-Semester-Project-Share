c9588afdcd71a881f4a32eb605604ed57e9ccb22
SplintDK <splint@splintnet.dk>
2015-11-25 10:30:09 +0100

    Updated enum for ProjectReturnType

--- Library/ProjectReturnType.cs
+++ Library/ProjectReturnType.cs
@@ -2,12 +2,12 @@
 {
     public enum ProjectReturnType
     {
-
-        IdMissing = 1,
-        TitleMissing = 2,
-        DescriptionMissing = 3,
-        ProjectFolderMissing = 4,
-        ProjectAdministratorUserMissing = 5
+        Success = 1,
+        IdMissing = 2,
+        TitleMissing = 3,
+        DescriptionMissing = 4,
+        ProjectFolderMissing = 5,
+        ProjectAdministratorUserMissing = 6
 
     }
 }
