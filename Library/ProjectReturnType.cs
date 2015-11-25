3aaf26f82ce96395a15ac082829ba49c4cd4a21e
SplintDK <splint@splintnet.dk>
2015-11-25 10:29:07 +0100

    Added enum for ProjectReturnType

---
+++ Library/ProjectReturnType.cs
@@ -0,0 +1,13 @@
+﻿namespace Library
+{
+    public enum ProjectReturnType
+    {
+
+        IdMissing = 1,
+        TitleMissing = 2,
+        DescriptionMissing = 3,
+        ProjectFolderMissing = 4,
+        ProjectAdministratorUserMissing = 5
+
+    }
+}
