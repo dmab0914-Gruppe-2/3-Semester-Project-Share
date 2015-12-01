1b3d045a8396868d42677356ddf4d8bf6bcc3726
Jacob Pedersen (Q11x) <q11x@q11x.net>
2015-12-01 09:48:33 +0100

    added IDbUser and implementation DbUser

---
+++ Server/Database/DbUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Database
+{
+    class DbUser : IDbUser
+    {
+        public Library.User FindUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Library.User[] FindUsersByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Library.User[] FindUsersByUserName(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Library.User[] FindAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server/Database/IDbUser.cs
@@ -0,0 +1,27 @@
+﻿using Library;
+
+namespace Server.Database
+{
+    public interface IDbUser
+    {
+
+        //Create
+        //bool AddUser(int id, string username, string password, string salt, UserType usertype, string email); //TODO Add later
+        //Read
+        User FindUserById(int id);
+        User[] FindUsersByEmail(string email); //Ment som email substring, listen kan bare indeholde 1 user hvis der kun findes 1 user
+        User[] FindUsersByUserName(string username); //Ment som substring, listen kan bare indeholde 1 user hvis der kun er 1
+        User[] FindAllUsers();
+        //Update
+        //bool UpdateUser(int id, string username, string password, string salt, UserType usertype, string email); //TODO Add later
+        //Delete
+        //bool DeleteUser(int id); //TODO Add later
+
+        //public int Id 
+        //public string Username 
+        //public string Password 
+        //public string Salt 
+        //public UserType Type 
+        //public string Email
+    }
+}
