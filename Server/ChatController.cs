2acb6c51b5a226dc79aeca112f903c3fd8ce9c1a
SplintDK <splint@splintnet.dk>
2015-12-10 09:22:15 +0100

    ChatController

---
+++ Server/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Database;
+
+namespace Server
+{
+    public class ChatController : IChatController
+    {
+        //private DbContext dbContext;
+        private readonly IDbChatMessage dbChatMessage;
+
+        public ChatController()
+        {
+            dbChatMessage = new DbChatMessage();
+        }
+
+        public bool SendMessage(Library.ChatMessage chatMessage, Library.File myFile)
+        {
+            return dbChatMessage.SendMessageToFile(chatMessage, myFile.Id);
+        }
+    }
+}
---
+++ Server/IChatController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public interface IChatController
+    {
+        bool SendMessage(Library.ChatMessage chatMessage, Library.File myFile);
+    }
+}
