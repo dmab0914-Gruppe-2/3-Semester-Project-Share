bbe41744b87843f92bff652dbcb7f11fed34cb2d
SplintDK <splint@splintnet.dk>
2015-12-10 10:53:14 +0100

    Added Chat service

---
+++ WebService/ChatService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Server;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatService" in both code and config file together.
+    public class ChatService : IChatService
+    {
+        IChatController chatController = new ChatController();
+        public bool SendMessage(Library.ChatMessage message, Library.File file)
+        {
+            return chatController.SendMessage(message, file);
+            throw new NotImplementedException();
+        }
+    }
+}
